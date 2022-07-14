220551e3109421abd12083a99014198ed4e4c740
Emlyn Davies <emlyn.john.davies@sintef.no>
2022-07-14 11:04:52 +0200

    add ingest_ais.py

---
+++ ingest_ais.py
@@ -0,0 +1,38 @@
+import os
+from datetime import datetime
+from influxdb import InfluxDBClient
+
+import config
+import util
+from ais import AIS
+# from adcp import ADCP
+# from lisst_200 import Lisst_200
+
+
+def main():
+
+    print("Starting running ingest_munkholmen.py at "
+          + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+    logger = util.init_logger(config.main_logfile, name='ingest_munkholmen')
+    logger.info("\n\n------ Starting sync/ingest.")
+
+    logger.info("Fetching the influxdb clients.")
+    admin_user, admin_pwd = util.get_influx_user_pwd(os.path.join(config.secrets_dir, 'influx_admin_credentials'))
+    clients = [
+        InfluxDBClient(config.az_influx_pc, 8086, admin_user, admin_pwd, 'oceanlab'),
+        InfluxDBClient(config.sintef_influx_pc, 8086, admin_user, admin_pwd, 'test'),
+    ]
+
+    ais = AIS(influx_clients=clients)
+    ais.ingest_l0()
+
+    # lisst = Lisst_200(influx_clients=clients)
+    # lisst.rsync_and_ingest()
+
+    # adcp = ADCP()
+    # adcp.rsync_and_ingest()
+
+
+if __name__ == "__main__":
+    main()

93b1cf5bf50e646d198faa11a1361b4ad60cdfb4
William Naylor <william.naylor@sintef.no>
2022-09-26 11:07:03 +0200

    starting cleaning the backups

--- inbox_folder_clearnup.py
+++ inbox_folder_clearnup.py
@@ -1,22 +1,27 @@
 import os
 import time
+import datetime
 
 import config
+import util_az
 
 '''
 Files rsynced/scp'ed onto the sintef network PC aren't cleaned up later.
 This delete any files older than 'days_to_keep' that are in 'folders_to_clean'.
 '''
 
-folders_to_clean = [
-    config.loggernet_inbox,
-    config.rsync_inbox,
-    config.rsync_inbox_adcp,
-]
-days_to_keep = 7
 
+def clean_inbox_folders(
+        folders_to_clean=[config.loggernet_inbox, config.rsync_inbox, config.rsync_inbox_adcp],
+        days_to_keep=7):
+    '''
+    Deletes all files in the folders 'folers_to_clean' oder than 'days_to_keep'.
 
-def main():
+    Parameters
+    ----------
+    folders_to_clean : list
+    days_to_keep : int
+    '''
     now = time.time()
     for folder in folders_to_clean:
         for f in os.listdir(folder):
@@ -26,5 +31,54 @@ def main():
                     os.remove(f)
 
 
+def clean_influx_backups(days=5, weeks=3, months=3):
+    '''
+    Deletes older backups to reduce storage space.
+    There will be days + weeks + months files kept.
+    Weeks aren't exact, just keeps 1st, 7th 14th and 21st of month.
+    Months just keeps the 1st of the month.
+
+    Example:
+    On 16th of July with input days=4, weeks=2, months=3 it would keep:
+        July 16, 15, 14, 13, 7, 1, June 1, May 1, April 1
+
+    Parameters
+    ----------
+    days : int
+    weeks : int
+    months : int
+    '''
+
+    day = datetime.datetime.now()
+    print(day.strftime('%Y%m%d'))
+    counter = 0
+    while True:
+        day = day - datetime.timedelta(days=1)
+        print('d', day.strftime('%Y%m%d'))
+        counter += 1
+        if counter >= (days - 1):
+            break
+    counter = 0
+    while True:
+        day = day - datetime.timedelta(days=1)
+        if day.strftime('%d') in ['01', '07', '14', '21']:
+            print('w', day.strftime('%Y%m%d'))
+            counter += 1
+        if counter >= weeks:
+            break
+    counter = 0
+    while True:
+        day = day - datetime.timedelta(days=1)
+        if day.strftime('%d') == '01':
+            print('m', day.strftime('%Y%m%d'))
+            counter += 1
+        if counter >= months:
+            break
+
+    util_az.container_ls('oceanlabdlcontainer', prefix='influx_backups/')
+    # util_az.upload_file('test.txt', 'test/test.txt', 'oceanlabdlcontainer')
+
+
 if __name__ == "__main__":
-    main()
+    # clean_inbox_folders()
+    clean_influx_backups()
---
+++ util_az.py
@@ -0,0 +1,84 @@
+import os
+import subprocess
+import logging
+
+import config
+
+logger = logging.getLogger('olmo.util_az')
+
+
+def upload_file(local_file, az_file, container, content_type='text/html', overwrite=True):
+    '''
+    Upload a file to the azure dl storage.
+
+    TODO: overwrite currently only can be 'True'
+
+    Parameters
+    ----------
+    local_file : str
+        Path to local file.
+    az_file : str
+        Path and name of file uploaded to azure.
+    container : str
+        Name of 'container' in azure to upload to.
+    content_type : str
+    overwrite : bool
+    '''
+
+    with open(os.path.join(config.secrets_dir, 'azure_token_web')) as f:
+        aztoken = f.read()
+    process = subprocess.Popen([
+        'az', 'storage', 'fs', 'file', 'upload',
+        '--source', local_file, '-p', az_file,
+        '-f', container, '--account-name', 'oceanlabdlstorage', '--overwrite',
+        '--content-type', content_type,
+        '--sas-token', aztoken[:-1]],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate(timeout=600)
+    # logger.info("STDOUT from 'az file upload':\n" + stdout.decode(errors="ignore"))
+    if process.returncode != 0:
+        # logger.error("az file upload failed. stderr:\n" + stderr.decode(errors="ignore"))
+        print("we got an error")
+        raise ValueError("process.returncode != 0.\n" + stderr.decode(errors="ignore"))
+    # logger.info('Backup, archive and transfer to azure completed successfully.')
+
+
+def container_ls(container, prefix=None):
+    '''
+    List the files in the given container. Prefix used for 'subfolders'.
+
+    Parameters
+    ----------
+    container : str
+    prefix : None or str
+    '''
+
+    # with open('/home/oceanlab/Secrets/azure_token_web') as f:
+    with open(os.path.join(config.secrets_dir, 'azure_token_web')) as f:
+        aztoken = f.read()
+
+    # az storage blob list --container-name container1 
+    # az storage fs list --account-name myadlsaccount --account-key 0000-0000
+    # process = subprocess.Popen([
+    #     'az', 'storage', 'fs', 'list',
+    #     '--prefix', prefix,  # '-c', container,
+    #     '--account-name', 'oceanlabdlstorage',
+    #     '--sas-token', aztoken[:-1]],
+    #     stdout=subprocess.PIPE,
+    #     stderr=subprocess.PIPE)
+    # az storage blob directory list -c MyContainer -d DestinationDirectoryPath --account-name MyStorageAccount
+    process = subprocess.Popen([
+        'az', 'storage', 'blob', 'directory', 'list',
+        '-c', container,
+        '-d', prefix,
+        '--account-name', 'oceanlabdlstorage',
+        '--sas-token', aztoken[:-1]],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate(timeout=60)
+    if process.returncode != 0:
+        print("We got an error.")
+        raise ValueError("process.returncode != 0.\n" + stderr.decode(errors="ignore"))
+    else:
+        print(stdout)
